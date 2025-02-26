21d8510fa86faeb500edd40f86c3485952e33fa3
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2025-02-26 14:19:45 +0100

    complete SignUp page functionality

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -1,17 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "../SignUp/SignUp.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="signup">
       <h1>Sing Up</h1>
 
-      <form>
-        <input type="text" placeholder="username" id="username" />
-        <input type="email" placeholder="email" id="email" />
-        <input type="password" placeholder="password" id="password" />
-        <button>Sign Up</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="disabled">
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
       </form>
 
       <div className="haveAnAccount">
@@ -20,6 +76,8 @@ const SignUp = () => {
           <span>Sign In</span>
         </Link>
       </div>
+
+      {error && <p>{error}</p>}
     </div>
   );
 };
