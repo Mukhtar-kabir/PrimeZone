ee3bd5881c8db133c2008037d7b7ffa0dc147b17
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2025-02-28 22:19:24 +0100

    complete profile page UI

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "../Profile/Profile.css";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="profile">
+      <h1>Profile</h1>
+      <img src={currentUser.avatar} alt="profile" />
+      <form>
+        <input type="username" placeholder="username" id="username" />
+        <input type="email" placeholder="email" id="email" />
+        <input type="password" placeholder="password" id="password" />
+        <button className="disabled">update</button>
+
+        <div className="items">
+          <span>Delete account</span>
+          <span>Sign Out</span>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
