420c393210bd57fd11fdd4ce64dc8b183e165a46
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2025-03-23 19:31:13 +0100

    update

--- api/index.js
+++ api/index.js
@@ -41,7 +41,7 @@ app.use("/api/auth", authRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 // app.use("/api/listing", listingRouter);
