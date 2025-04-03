db8d65ccb9e9ca658145d4293550ce340096779e
Mukhtar Kabir <mukhtarkabirmuhammad32@gmail.com>
2025-04-03 06:08:01 +0100

    Update SignUp.jsx

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -22,7 +22,7 @@ const SignUp = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch(`{BASE_URL}/api/auth/signup`, {
+      const res = await fetch(`${BASE_URL}/api/auth/signup`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
