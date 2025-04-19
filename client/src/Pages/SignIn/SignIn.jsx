01d23e3790de1117d0b6493e657bd878832da769
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2025-04-19 07:22:51 +0100

    update

--- client/src/Pages/SignIn/SignIn.jsx
+++ client/src/Pages/SignIn/SignIn.jsx
@@ -139,7 +139,7 @@ const SignIn = () => {
     e.preventDefault();
     dispatch(signInStart());
     try {
-      const res = await fetch(`/api/auth/signin`, {
+      const res = await fetch(`${BASE_URL}/api/auth/signin`, {
         method: "POST",
         credentials: "include",
         headers: {
